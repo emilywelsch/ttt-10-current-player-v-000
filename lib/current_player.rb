321096fac0b70a1c11e2c121f164c0700278c90f
Emily Welsch <emwelsch@gmail.com>
2018-01-30 20:20:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,8 @@
+board = []
 def turn_count(board)
-  # how many occupied spaces are there on the board?
+  # count the number of occupied spaces on the board
   # if there are an odd number then it's O's turn, even number then it's X's turn
+  
   user_markers = ["X","O","X","O","X","O","X","O","X"]
 end
 
