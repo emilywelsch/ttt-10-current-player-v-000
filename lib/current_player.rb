0294b4c8a02121b777deb395ae03c5e488081664
Emily Welsch <emwelsch@gmail.com>
2018-01-30 20:21:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,9 @@
 board = []
 def turn_count(board)
   # count the number of occupied spaces on the board
-  # if there are an odd number then it's O's turn, even number then it's X's turn
-  
   user_markers = ["X","O","X","O","X","O","X","O","X"]
 end
 
 def current_player(board)
-  # what is the turn count
+  # if turn_count is an odd number then it's O's turn, even number then it's X's turn
 end
